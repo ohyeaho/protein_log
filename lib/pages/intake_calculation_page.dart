d4593fc342c0d1022b1c6f011605b5f5ba611aaa
ohyeaho <ohyeah7275@gmail.com>
2022-04-30 19:14:51 +0900

    add admob to intake_calculation_page

--- lib/pages/intake_calculation_page.dart
+++ lib/pages/intake_calculation_page.dart
@@ -46,217 +46,240 @@ class _IntakeCalculationPageState extends State<IntakeCalculationPage> {
       appBar: AppBar(
         title: Text('たんぱく質目安計算'),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(40.0),
-        child: Column(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Card(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('体重'),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              weight--;
-                            });
-                          },
-                          icon: Icon(Icons.remove_circle_outline),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.baseline,
-                          textBaseline: TextBaseline.alphabetic,
-                          children: [
-                            Text(
-                              weight.toString(),
-                              style: TextStyle(fontSize: 30),
-                            ),
-                            SizedBox(
-                              width: 5.0,
-                            ),
-                            Text('kg')
-                          ],
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            setState(() {
-                              weight++;
-                            });
-                          },
-                          icon: Icon(Icons.add_circle_outline),
-                        ),
-                      ],
-                    ),
-                    Container(
-                      width: double.infinity,
-                      child: CupertinoSlider(
-                        value: weight.toDouble(),
-                        min: 0.0,
-                        max: 150.0,
-                        onChanged: (value) {
-                          setState(() {
-                            weight = value.toInt();
-                          });
-                        },
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 8.0),
-                  child: OutlinedButton(
-                    onPressed: () {
-                      showModalBottomSheet(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return Container(
-                            height: MediaQuery.of(context).size.height / 3,
-                            child: GestureDetector(
-                              onTap: () {
-                                Navigator.pop(context);
-                              },
-                              child: CupertinoPicker(
-                                itemExtent: 30,
-                                children: itemStrings.map(_pickerItem).toList(),
-                                onSelectedItemChanged: (index) {
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: Card(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text('体重'),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              IconButton(
+                                onPressed: () {
                                   setState(() {
-                                    selectText = itemStrings[index];
-                                    _index = index;
+                                    weight--;
                                   });
                                 },
+                                icon: Icon(
+                                  Icons.remove_circle_outline,
+                                  size: 35,
+                                ),
                               ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.baseline,
+                                textBaseline: TextBaseline.alphabetic,
+                                children: [
+                                  Text(
+                                    weight.toString(),
+                                    style: TextStyle(fontSize: 30),
+                                  ),
+                                  SizedBox(
+                                    width: 5.0,
+                                  ),
+                                  Text('kg')
+                                ],
+                              ),
+                              IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    weight++;
+                                  });
+                                },
+                                icon: Icon(
+                                  Icons.add_circle_outline,
+                                  size: 35,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Container(
+                            width: double.infinity,
+                            child: CupertinoSlider(
+                              value: weight.toDouble(),
+                              min: 0.0,
+                              max: 150.0,
+                              onChanged: (value) {
+                                setState(() {
+                                  weight = value.toInt();
+                                });
+                              },
                             ),
-                          );
-                        },
-                      );
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                          child: Text(
-                            selectText,
-                            maxLines: 2,
-                          ),
-                        ),
-                        Icon(Icons.arrow_drop_down),
-                      ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-            Expanded(
-              flex: 1,
-              child: Card(
-                  child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Text('10代ですか？'),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Row(
-                        children: [
-                          Radio(
-                            value: 1,
-                            groupValue: ageId,
-                            onChanged: (value) {
-                              setState(() {
-                                radioButtonItem = 'はい';
-                                ageId = 1;
-                              });
-                            },
+                  Expanded(
+                    flex: 1,
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 30.0, horizontal: 8.0),
+                        child: OutlinedButton(
+                          onPressed: () {
+                            showModalBottomSheet(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return Container(
+                                  height: MediaQuery.of(context).size.height / 3,
+                                  child: GestureDetector(
+                                    onTap: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: CupertinoPicker(
+                                      itemExtent: 30,
+                                      children: itemStrings.map(_pickerItem).toList(),
+                                      onSelectedItemChanged: (index) {
+                                        setState(() {
+                                          selectText = itemStrings[index];
+                                          _index = index;
+                                        });
+                                      },
+                                    ),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Expanded(
+                                child: Text(
+                                  selectText,
+                                  maxLines: 2,
+                                ),
+                              ),
+                              Icon(Icons.arrow_drop_down),
+                            ],
                           ),
-                          Text(
-                            'はい',
-                            style: new TextStyle(fontSize: 17.0),
-                          ),
-                        ],
+                        ),
                       ),
-                      Row(
-                        children: [
-                          Radio(
-                            value: 2,
-                            groupValue: ageId,
-                            onChanged: (value) {
-                              setState(() {
-                                radioButtonItem = 'いいえ';
-                                ageId = 2;
-                              });
-                            },
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Card(
+                        child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text('10代ですか？'),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              children: [
+                                Radio(
+                                  value: 1,
+                                  groupValue: ageId,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      radioButtonItem = 'はい';
+                                      ageId = 1;
+                                    });
+                                  },
+                                ),
+                                Text(
+                                  'はい',
+                                  style: new TextStyle(fontSize: 17.0),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Radio(
+                                  value: 2,
+                                  groupValue: ageId,
+                                  onChanged: (value) {
+                                    setState(() {
+                                      radioButtonItem = 'いいえ';
+                                      ageId = 2;
+                                    });
+                                  },
+                                ),
+                                Text(
+                                  'いいえ',
+                                  style: new TextStyle(fontSize: 17.0),
+                                ),
+                              ],
+                            ),
+                          ],
+                        )
+                      ],
+                    )),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 35.0),
+                      child: Container(
+                        width: double.infinity,
+                        child: ElevatedButton(
+                          onPressed: () {
+                            ProteinFormula calc = ProteinFormula(
+                              weight: weight,
+                              motion: _index,
+                              ageId: ageId,
+                            );
+                            setState(() {
+                              result = calc.resultProtein()!;
+                            });
+                          },
+                          child: Text(
+                            '計算',
+                            style: TextStyle(fontSize: 25),
                           ),
-                          Text(
-                            'いいえ',
-                            style: new TextStyle(fontSize: 17.0),
-                          ),
-                        ],
+                        ),
                       ),
-                    ],
-                  )
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: Center(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                '１日に必要なたんぱく質目安量',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              FittedBox(
+                                child: Text(
+                                  '${result}g',
+                                  style: TextStyle(
+                                    fontSize: 50,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  maxLines: 1,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-              )),
-            ),
-            Expanded(
-              flex: 1,
-              child: Padding(
-                padding: const EdgeInsets.symmetric(vertical: 30.0),
-                child: Container(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      ProteinFormula calc = ProteinFormula(weight: weight, motion: _index, ageId: ageId);
-                      setState(() {
-                        result = calc.resultProtein()!;
-                      });
-                    },
-                    child: Text(
-                      '計算',
-                      style: TextStyle(fontSize: 25),
-                    ),
-                  ),
-                ),
               ),
             ),
-            Expanded(
-              flex: 1,
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          '１日に必要なたんぱく質目安量',
-                          style: TextStyle(fontSize: 20),
-                        ),
-                        FittedBox(
-                          child: Text(
-                            '${result}g',
-                            style: TextStyle(fontSize: 50, fontWeight: FontWeight.w600),
-                            maxLines: 1,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+          Container(
+            height: height * 0.06,
+            child: AdWidget(ad: myBanner),
+          ),
+        ],
       ),
     );
   }
